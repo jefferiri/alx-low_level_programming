a1b959b8bd9ed8522febb9d4760a307b20c72614
jefferiri <driri14@gmail.com>
2023-03-10 10:24:25 -0800

    Static libraries commit

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,8 +1,5 @@
 #ifndef FILE_MAIN
 #define FILE_MAIN
-
-/*copy list from task*/
-
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -23,6 +20,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
 
